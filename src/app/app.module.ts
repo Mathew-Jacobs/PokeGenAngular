ff0e01135004fdf9386c4aab50118b4239b62b6a
mathew.jacobs <mathew.jacobs@sinclair.edu>
2019-08-01 14:56:57 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { 
+  MatToolbarModule,
+  MatButtonModule
+} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/registration/registration.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.scss']
+})
+export class RegistrationComponent implements OnInit {
+
+  private _registerForm: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) { 
+    this.createForm();
+  }
+
+  ngOnInit() {
+  }
+
+  createForm() {
+    this._registerForm = this._formBuilder.group({
+      username: new FormControl,
+      password: new FormControl,
+      confirmPassword: new FormControl
+    });
+  }
+}
